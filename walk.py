0c49f62fcba067ab15aa566a226d21dde0450a18
Shruti Sharma <42216801+shruti1210@users.noreply.github.com>
2022-11-14 14:01:17 -0500

    Update walk.py

--- walk.py
+++ walk.py
@@ -47,7 +47,7 @@ theta40 = 183
 while True:
     #motion for the left leg 
     servo1.move(theta10)
-    servo2.move(theta20 + sin(t) * 10)
+    servo2.move(theta20 + sin(t) * 20)
     
     print("\n at time t", t)
     
@@ -57,7 +57,7 @@ while True:
     
     #motion for the right leg
     servo3.move(theta30)
-    servo4.move(theta40 + cos(t) * 10)
+    servo4.move(theta40 + cos(t) * 20)
     
     print("angle for upper motor on right leg is", servo3.get_physical_angle())
     print("angle for lower motor on right leg is", servo4.get_physical_angle())
