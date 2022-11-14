f537964fb98b0a9ca1aeff2f01a837fed5ddb8a8
Shruti Sharma <42216801+shruti1210@users.noreply.github.com>
2022-11-13 21:54:47 -0500

    get_initial_motor_positions

--- walk.py
+++ walk.py
@@ -12,6 +12,12 @@ try:
     #set limits for the servo angles left leg
     servo1.set_angle_limits(0, 240)
     servo2.set_angle_limits(0, 240)
+    
+    #get initial servo positions for left leg
+    theta1 = servo1.get_physical_angle()
+    print("angle for upper motor on left leg is", theta1)
+    theta2 = servo2.get_physical_angle()
+    print("angle for lower motor on left leg is", theta2)
 
     #right leg
     servo3 = LX16A(3)
@@ -19,7 +25,13 @@ try:
 
     #set limits for the servo angles right leg 
     servo3.set_angle_limits(0, 240)
-    servo4.set_angle_limits(0, 240)    
+    servo4.set_angle_limits(0, 240) 
+    
+    #get initial servo positions for right leg
+    theta3 = servo3.get_physical_angle()
+    print("angle for upper motor on right leg is", theta3)
+    theta4 = servo4.get_physical_angle()
+    print("angle for lower motor on right leg is", theta4)
 
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
@@ -38,4 +50,4 @@ while True:
     servo4.move(cos(t) * 10 + 10)
     time.sleep(0.1)
 
-    t += 0.1
+    t += 0.01
